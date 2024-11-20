cede5f528584ba9e3a6f0b5fb6cd815583bffdc1
Jefferson Lucas <jefferson.altere@gmail.com>
2024-11-20 12:56:00 -0300

    Salva JSON no S3

--- src/main/java/com/urlshortner/Main.java
+++ src/main/java/com/urlshortner/Main.java
@@ -1,8 +1,11 @@
-package com.urlshortner;
+package com.urlshortener;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 
 import java.util.HashMap;
@@ -12,6 +15,9 @@ import java.util.UUID;
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    private  final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
@@ -28,8 +34,24 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
 
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
+        long expirationTimeSeconds = Long.parseLong(expirationTime);
 
         String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        UrlData urlData = new UrlData(originalUrl, expirationTimeSeconds);
+
+        try {
+            String urlDataJson = objectMapper.writeValueAsString(urlData);
+
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket("url-shortener-bucket-lambda")
+                    .key(shortUrlCode + ".json")
+                    .build();
+            s3Client.putObject(putObjectRequest, RequestBody.fromString(urlDataJson));
+
+        } catch (Exception exception) {
+            throw new RuntimeException("Error saving data to S3: " + exception.getMessage(),  exception);
+        }
 
         Map<String, String> response = new HashMap<>();
 
